920fc45da1f0b941304a0457cc739554b122025f
Puliphat <myty.2543@gmail.com>
2025-05-19 18:47:39 +0700

    update next.js at skill

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from './Skills.module.css'
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGitAlt, FaNode, FaDocker } from 'react-icons/fa6'
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGitAlt, FaNode, FaDocker ,FaPhp  } from 'react-icons/fa6'
 import { GrMysql } from "react-icons/gr";
 import { BiLogoTailwindCss } from "react-icons/bi";
 import { SiPostman, SiExpress } from "react-icons/si";
+import { RiNextjsFill } from "react-icons/ri";
 import  useInView  from '../../hooks/useInView'
 
 
@@ -11,15 +12,18 @@ const frontendSkills = [
   { name: "HTML", icon: <FaHtml5 style={{ color: '#E34F26' }} />, level: 100 },
   { name: "CSS", icon: <FaCss3Alt style={{ color: '#4a90e2' }} />, level: 100 },
   { name: "JavaScript", icon: <FaJs style={{ color: '#F7DF1E' }} />, level: 100 },
-  { name: "React.js", icon: <FaReact style={{ color: '#61DAFB' }} />, level: 100 },
+  { name: "React.js", icon: <FaReact style={{ color: '#61DAFB' }} />, level: 90 },
+  { name: "Next.js", icon: <RiNextjsFill style={{ color: '#000000' }} />, level: 20 },
   { name: "TailwindCSS", icon: <BiLogoTailwindCss style={{ color: '#06B6D4' }} />, level: 85 }
 ];
 
 const backendSkills = [
-  { name: "Node.js", icon: <FaNode style={{ color: '#339933' }} />, level: 100 },
-  { name: "Express.js", icon: <SiExpress style={{ color: '#000000' }} />, level: 100 },
+  { name: "Node.js", icon: <FaNode style={{ color: '#339933' }} />, level: 90 },
+  { name: "Express.js", icon: <SiExpress style={{ color: '#000000' }} />, level: 90 },
   { name: "MySQL", icon: <GrMysql style={{ color: '#000000' }} />, level: 90 },
-  { name: "Git", icon: <FaGitAlt style={{ color: '#F05032' }} />, level: 85 },
+  { name: "PHP", icon: <FaPhp style={{ color: '#777BB4' }} />, level: 60 },
+  { name: "Next.js", icon: <RiNextjsFill style={{ color: '#000000' }} />, level: 20 },
+  { name: "Git", icon: <FaGitAlt style={{ color: '#F05032' }} />, level: 80 },
   { name: "Postman", icon: <SiPostman style={{ color: '#FF6C37' }} />, level: 100 },
   { name: "Docker", icon: <FaDocker style={{ color: '#2496ED' }} />, level: 85 }
 ];
